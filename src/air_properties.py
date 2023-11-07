b70bc7c00a794c8103d092598c55d5751dbfc806
R-Rijnbeek <robert270384@gmail.com>
2023-11-07 14:06:21 +0100

    cleanup of code

--- src/air_properties.py
+++ src/air_properties.py
@@ -7,10 +7,10 @@ def vapourpressure(temp: int | float) -> float:
     return 10**(8.07131 - (1730.63/(233.426+temp)))*133.322
 
 @argument_check((float,int),(float,int))
-def density_air(temp: int | float,rh: int | float) -> float:
+def density_air(temp: int | float, rh: int | float) -> float:
     temp=float(temp)
     rh=float(rh)
-    return 219.56*(1 +absolutehumidity_kg_air(temp,rh)/1000)/((0.622 + absolutehumidity_kg_air(temp,rh)/1000)*(temp + 273))
+    return 219.56*(1 +absolutehumidity_kg_air(temp, rh)/1000)/((0.622 + absolutehumidity_kg_air(temp, rh)/1000)*(temp + 273))
 
 @argument_check((float,int),(float,int))
 def absolutehumidity_kg_air(temp: int | float,rh: int | float) -> float:
@@ -20,73 +20,64 @@ def absolutehumidity_kg_air(temp: int | float,rh: int | float) -> float:
     return 0.622*waterdd*(rh/100)/(101325. - waterdd*(rh/100))*1000
 
 @argument_check((float,int),(float,int))
-def absolutehumidity_m3_air(temp: int | float,rh: int | float) -> float:
+def absolutehumidity_m3_air(temp: int | float, rh: int | float) -> float:
     temp=float(temp)
     rh=float(rh)
-    return (absolutehumidity_kg_air(temp,rh)*density_air(temp,rh))
+    return (absolutehumidity_kg_air(temp, rh)*density_air(temp, rh))
     
 @argument_check((float,int),(float,int))
-def entalpie_kg_air(temp: int | float,rh: int | float) -> float:
+def entalpie_kg_air(temp: int | float, rh: int | float) -> float:
     temp=float(temp)
     rh=float(rh)
-    return ((1.005*temp) + absolutehumidity_kg_air(temp,rh)*(2500.6 + (1.85*temp))/1000)
+    return ((1.005*temp) + absolutehumidity_kg_air(temp, rh)*(2500.6 + (1.85*temp))/1000)
 
 @argument_check((float,int),(float,int))
-def entalpie_m3_air(temp: int | float,rh: int | float) -> float:
+def entalpie_m3_air(temp: int | float, rh: int | float) -> float:
     temp=float(temp)
     rh=float(rh)
-    return (entalpie_kg_air(temp,rh)*density_air(temp,rh))
+    return (entalpie_kg_air(temp, rh)*density_air(temp, rh))
     
-@argument_check((float,int),(float,int))
+@argument_check((float,int),(float, int))
 def moisuredeficit_kg_air(temp: int | float,rh: int | float) -> float:
     temp=float(temp)
     rh=float(rh)
-    return (absolutehumidity_kg_air(temp,100)-absolutehumidity_kg_air(temp,rh))
+    return (absolutehumidity_kg_air(temp, 100.)-absolutehumidity_kg_air(temp, rh))
 
 @argument_check((float,int),(float,int))
-def moisuredeficit_m3_air(temp: int | float,rh: int | float) -> float:
+def moisuredeficit_m3_air(temp: int | float, rh: int | float) -> float:
     temp=float(temp)
     rh=float(rh)
-    return (moisuredeficit_kg_air(temp,rh)*density_air(temp,rh))
+    return (moisuredeficit_kg_air(temp, rh)*density_air(temp, rh))
 
 @argument_check((float,int),(float,int))
-def dew_point_factor(temp: int | float,rh: int | float) -> float:
+def dew_point_factor(temp: int | float, rh: int | float) -> float:
     temp=float(temp)
     rh=float(rh)
     return (sympy.log(rh/100.) + 17.67*temp/(243.5 + temp))
 
 @argument_check((float,int),(float,int))
-def dew_point_temperature(temp: int | float,rh: int | float) -> float:
+def dew_point_temperature(temp: int | float, rh: int | float) -> float:
     temp=float(temp)
     rh=float(rh)
-    return (243.5*dew_point_factor(temp,rh))/(17.67 - dew_point_factor(temp,rh))
+    return (243.5*dew_point_factor(temp, rh))/(17.67 - dew_point_factor(temp, rh))
 
 
 if __name__ == '__main__':
 
     try:
-        temp = 5.
+        temp = 45.
         rh = 90.
 
         vp = vapourpressure(temp)
-
-        ad = density_air(temp,rh)
-
-        ab_hu_kg =absolutehumidity_kg_air(temp,rh)
-
-        ab_hu_m3 = absolutehumidity_m3_air(temp,rh)
-
-        en_kg = entalpie_kg_air(temp,rh)
-
-        en_m3 = entalpie_m3_air(temp,rh)
-
+        ad = density_air(temp, rh)
+        ab_hu_kg =absolutehumidity_kg_air(temp, rh)
+        ab_hu_m3 = absolutehumidity_m3_air(temp, rh)
+        en_kg = entalpie_kg_air(temp, rh)
+        en_m3 = entalpie_m3_air(temp, rh)
         mois_def_kg = moisuredeficit_kg_air(temp,rh)
-
-        mois_def_m3 = moisuredeficit_m3_air(temp,rh)
-
-        dewpoint = dew_point_factor(temp,rh)
-
-        dewpoint_temp = dew_point_temperature(temp,rh)
+        mois_def_m3 = moisuredeficit_m3_air(temp, rh)
+        dewpoint = dew_point_factor(temp, rh)
+        dewpoint_temp = dew_point_temperature(temp, rh)
         
         print(f"vp => {vp}")
         print(f"ad => {ad}")
