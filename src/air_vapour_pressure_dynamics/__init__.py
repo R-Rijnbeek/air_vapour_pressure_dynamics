59d544da151cbda53fce27a53e96283acef06c38
R-Rijnbeek <robert270384@gmail.com>
2023-12-13 15:50:56 +0100

    Finished putting output type selector

--- src/air_vapour_pressure_dynamics/__init__.py
+++ src/air_vapour_pressure_dynamics/__init__.py
@@ -8,73 +8,79 @@ __version__ = "1.5.1"
 
 # ======== IMPORTS ===========
 
-from .utils import ( getFunctionBackName, 
+from utils import ( getFunctionBackName, 
                     argumentChecker_2var, 
                     argumentChecker_1var, 
                     setArgCheck,
+                    setApplyU,
                     UnitFloat
                     )
 
-# ======= BASE FUNCTIONS =====
+# ========== SETTERS =========
 
 def setArgumentCheck(bool: bool) -> None:
     setArgCheck(bool)
 
-def vapourpressure(temp: int | float) -> UnitFloat:
+def setApplyUnits(bool: bool) -> None:
+    setApplyU(bool)
+
+# ======= BASE FUNCTIONS =====
+
+def vapourpressure(temp: int | float) -> UnitFloat | float:
     """
     Function that calculate the vapour pressure of the air with argument the temperature (temp).
     """
     return argumentChecker_1var(temp, function=getFunctionBackName())
 
-def density_air(temp: int | float, rh: int | float) -> UnitFloat:
+def density_air(temp: int | float, rh: int | float) -> UnitFloat | float:
     """
     Function that calculate the desity of the air kg/m3 witha as argument the temperature (temp) and relative humidity (rh).
     """
     return argumentChecker_2var(temp, rh, function=getFunctionBackName())
 
-def absolutehumidity_kg_air(temp: int | float, rh: int | float) -> UnitFloat:
+def absolutehumidity_kg_air(temp: int | float, rh: int | float) -> UnitFloat | float:
     """
     Function that calculate the absolute humidity of 1 kg the air witha as argument the temperature (temp) and relative humidity (rh).
     """
     return argumentChecker_2var(temp, rh, function=getFunctionBackName())
 
-def absolutehumidity_m3_air(temp: int | float, rh: int | float) -> UnitFloat:
+def absolutehumidity_m3_air(temp: int | float, rh: int | float) -> UnitFloat | float:
     """
     Function that calculate the absolute humidity of 1 cubic meter of air witha as argument the temperature (temp) and relative humidity (rh).
     """
     return argumentChecker_2var(temp, rh, function=getFunctionBackName())
 
-def entalpie_kg_air(temp: int | float, rh: int | float) -> UnitFloat:
+def entalpie_kg_air(temp: int | float, rh: int | float) -> UnitFloat | float:
     """
     Function that calculate the entalpie of 1 kilogram air with as argument the temperature (temp) and relative humidity (rh).
     """
     return argumentChecker_2var(temp, rh, function=getFunctionBackName())
 
-def entalpie_m3_air(temp: int | float, rh: int | float) -> UnitFloat:
+def entalpie_m3_air(temp: int | float, rh: int | float) -> UnitFloat | float:
     """
     Function that calculate the entalpie of 1 cubic meter air with as argument the temperature (temp) and relative humidity (rh).
     """
     return argumentChecker_2var(temp, rh, function=getFunctionBackName())
 
-def moisuredeficit_kg_air(temp: int | float, rh: int | float) -> UnitFloat:
+def moisuredeficit_kg_air(temp: int | float, rh: int | float) -> UnitFloat | float:
     """
     Function that calculate the moisure dficit of 1 kg air with as argument the temperature (temp) and relative humidity (rh).
     """
     return argumentChecker_2var(temp, rh, function=getFunctionBackName())
 
-def moisuredeficit_m3_air(temp: int | float, rh: int | float) -> UnitFloat:
+def moisuredeficit_m3_air(temp: int | float, rh: int | float) -> UnitFloat | float:
     """
     Function that calculate the moisure dficit of 1 cubic meter air with as argument the temperature (temp) and relative humidity (rh).
     """
     return argumentChecker_2var(temp, rh, function=getFunctionBackName())
 
-def dew_point_factor(temp: int | float, rh: int | float) -> UnitFloat:
+def dew_point_factor(temp: int | float, rh: int | float) -> UnitFloat | float:
     """
     Function that calculate the dewpoint factor of air with as argument the temperature (temp) and relative humidity (rh).
     """
     return argumentChecker_2var(temp, rh, function=getFunctionBackName())
 
-def dew_point_temperature(temp: int | float, rh: int | float) -> UnitFloat:
+def dew_point_temperature(temp: int | float, rh: int | float) -> UnitFloat | float:
     """
     Function that calculate the dewpoint temperature of air with as argument the temperature (temp) and relative humidity (rh).
     """
@@ -83,4 +89,8 @@ def dew_point_temperature(temp: int | float, rh: int | float) -> UnitFloat:
 
 if __name__ == '__main__':
 
-    pass
+
+    setApplyUnits(False)
+
+    a = dew_point_temperature(45,90)
+    print(a)
