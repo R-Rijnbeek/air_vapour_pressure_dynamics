36b22aa124c74d6ef93b720bb0533695e9021884
R-Rijnbeek <robert270384@gmail.com>
2023-12-26 23:56:54 +0100

    cleanup

--- test/sympy_testing.py
+++ test/sympy_testing.py
@@ -2,10 +2,10 @@
 
 # ====== IMPORTS ===============
 
-import sys
-sys.path.append('.')
-from src.air_vapour_pressure_dynamics import (  
-#from air_vapour_pressure_dynamics import (
+#import sys
+#sys.path.append('.')
+#from src.air_vapour_pressure_dynamics import (  
+from air_vapour_pressure_dynamics import (
                                             vapourpressure, 
                                             density_air, 
                                             absolutehumidity_kg_air, 
