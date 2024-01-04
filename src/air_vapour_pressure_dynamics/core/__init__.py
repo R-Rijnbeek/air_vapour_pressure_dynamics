4d3ea7996f41e9e3f64583145f78fbb3583e3fed
R-Rijnbeek <robert270384@gmail.com>
2024-01-04 16:17:36 +0100

    cleanup of code

--- src/air_vapour_pressure_dynamics/core/__init__.py
+++ src/air_vapour_pressure_dynamics/core/__init__.py
@@ -14,18 +14,15 @@ from ..extra._float import UnitFloat
     
 # ======= FUNCTIONS ===========
 
-def inputChanger(arg):
+def _inputChanger(arg):
     if isinstance(arg,int):
         return float(arg)
     if ( isNumpyValue(arg) ):
         return arg.astype(numpyFloat64)
     return arg
 
-def inputAdapter(*args):
-    return tuple(inputChanger(arg) for arg in args)
-
-def getFunctionBackName():
-    return inspect.currentframe().f_back.f_code.co_name
+def _inputAdapter(*args):
+    return tuple(_inputChanger(arg) for arg in args)
 
 def _getFunctionByName(functionName):
     return FUNCTION_CONFIG[functionName]["function"]
@@ -33,7 +30,7 @@ def _getFunctionByName(functionName):
 def _getUnitsByName(functionName):
     return FUNCTION_CONFIG[functionName]["unit"]
 
-def MakeUpOutput(value, functionName):
+def _makeUpOutput(value, functionName):
     if SETTINGS.APPLY_UNITS :
         if isinstance(value, float) :
             return UnitFloat(value, _getUnitsByName(functionName))
@@ -43,40 +40,8 @@ def MakeUpOutput(value, functionName):
             return UnitSympyExpression(value, _getUnitsByName(functionName))
     return value
 
-
-def argumentChecker_2var(TEMP, HR, function="density_air"):
-    if SETTINGS.ARGUMENT_CHECK :
-        formats = [int, float]
-        if SETTINGS.NUMPY_DETECTED :
-            formats.append(numpyArray)
-        if SETTINGS.SYMPY_DETECTED :
-            formats.append(sympySymbol)
-        formats = (tuple(formats),tuple(formats))
-        @argument_check(*formats)
-        def wrapper(temp, hr):
-            return _getFunctionByName(function)(temp, hr)
-    else:
-        def wrapper(temp, hr):
-            return _getFunctionByName(function)(temp, hr)
-    (TEMP, HR,) = inputAdapter(TEMP, HR)
-    return MakeUpOutput(wrapper(TEMP, HR),function)
-    
-def argumentChecker_1var(TEMP, function="density_air"):
-    if SETTINGS.ARGUMENT_CHECK :
-        formats = [int, float]
-        if SETTINGS.NUMPY_DETECTED :
-            formats.append(numpyArray)
-        if SETTINGS.SYMPY_DETECTED :
-            formats.append(sympySymbol)
-        formats = (tuple(formats))
-        @argument_check(formats)
-        def wrapper(temp):
-            return _getFunctionByName(function)(temp)
-    else:
-        def wrapper(temp):
-            return _getFunctionByName(function)(temp)
-    (TEMP,) = inputAdapter(TEMP)
-    return MakeUpOutput(wrapper(TEMP),function)
+def getFunctionBackName():
+    return inspect.currentframe().f_back.f_code.co_name
 
 def controller(*args, function = "density_air"):
     argument_lenght = len(args)
@@ -98,13 +63,11 @@ def controller(*args, function = "density_air"):
             def wrapper(*arguments):
                 return _getFunctionByName(function)(*arguments)
             
-        ARGS = inputAdapter(*args)
-        return MakeUpOutput(wrapper(*ARGS),function)
+        ARGS = _inputAdapter(*args)
+        return _makeUpOutput(wrapper(*ARGS),function)
 
     else:
         raise AssertionError("number of argument must be 1 or 2")
-
-
 
 if __name__ == '__main__':
     
